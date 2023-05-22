d1857cd63662c180889353b96dd3f368917b5898
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-22 19:28:40 -0400

    sadsadsadasdasd

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-async function downloadprepare() {
-  var stream = { "1": window.audioStream1, "2": window.audioStream2 }
-  let blob = new Blob(stream, { type: 'audio/mpeg-3' });
-  recordedAudio.src = URL.createObjectURL(blob);
-  recordedAudio.controls = true;
-  recordedAudio.autoplay = true;
-}
+navigator.mediaDevices.getUserMedia({audio:true})
+.then(stream => {handlerFunction(stream)})
+      function handlerFunction(stream) {
+      rec = new MediaRecorder(stream);
+      rec.ondataavailable = e => {
+        audioChunks.push(e.data);
+        if (rec.state == "inactive"){
+          let blob = new Blob(audioChunks,{type:'audio/mpeg-3'});
+          recordedAudio.src = URL.createObjectURL(blob);
+          recordedAudio.controls=true;
+          recordedAudio.autoplay=true;
+          sendData(blob)
+        }
+      }
+    }
+          function sendData(data) {}
+
+  record.onclick = e => {
+    console.log('I was clicked')
+    record.disabled = true;
+    record.style.backgroundColor = "blue"
+    stopRecord.disabled=false;
+    audioChunks = [];
+    rec.start();
+  }
+  stopRecord.onclick = e => {
+    console.log("I was clicked")
+    record.disabled = false;
+    stop.disabled=true;
+    record.style.backgroundColor = "red"
+    rec.stop();
+  }
 
 function getLocalStream() {
   const audioSource = audioInputSelect1.value;
